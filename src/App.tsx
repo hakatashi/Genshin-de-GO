6e830b0c2c8a2caa79ade3324265d859ffc3a9de
Koki Takahashi <hakatasiloving@gmail.com>
2024-01-16 22:38:09 +0000

    Install react-pixi

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Stage, Container } from '@pixi/react';
 import './App.css';
+import { Bunny } from './Bunny';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Stage width={1000} height={1000} options={{ backgroundAlpha: 0 }}>
+          <Container x={150} y={150}>
+            <Bunny />
+          </Container>
+        </Stage>
       </header>
     </div>
   );
---
+++ src/Bunny.tsx
@@ -0,0 +1,32 @@
+import { Sprite, _ReactPixi, useTick } from '@pixi/react';
+import { useRef, useReducer } from 'react';
+
+type SpriteProps = Partial<_ReactPixi.ISprite>;
+
+const reducer = (state: SpriteProps, action: {type: string, data: SpriteProps}) => {
+  if (action.type === 'update') {
+    return action.data;
+  }
+  return state;
+};
+
+export const Bunny = () => {
+  const [motion, update] = useReducer(reducer, {});
+  const iter = useRef(0);
+
+  useTick((delta) => {
+    const i = (iter.current += 0.05 * delta);
+
+    update({
+      type: 'update',
+      data: {
+        x: Math.sin(i) * 300 + 500,
+        y: Math.sin(i / 1.5) * 300 + 500,
+        rotation: Math.sin(i) * Math.PI,
+        anchor: Math.sin(i / 2),
+      },
+    });
+  });
+
+  return <Sprite image="/logo192.png" {...motion} />;
+};
