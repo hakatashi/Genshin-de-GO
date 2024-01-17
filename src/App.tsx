f6ea94526de17ee12f5a5b5dc65eb53f0bf33dd2
Koki Takahashi <hakatasiloving@gmail.com>
2024-01-17 13:39:15 +0000

    Setup lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
-import { Stage, Container } from '@pixi/react';
+import {Stage, Container} from '@pixi/react';
 import './App.css';
-import { Bunny } from './Bunny';
+import {Bunny} from './Bunny';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Stage width={1000} height={1000} options={{ backgroundAlpha: 0 }}>
-          <Container x={150} y={150}>
-            <Bunny />
-          </Container>
-        </Stage>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+	<div className="App">
+		<header className="App-header">
+			<Stage width={1000} height={1000} options={{backgroundAlpha: 0}}>
+				<Container x={150} y={150}>
+					<Bunny/>
+				</Container>
+			</Stage>
+		</header>
+	</div>
+);
 
 export default App;
